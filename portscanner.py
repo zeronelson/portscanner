ea43d432537495a56a5945848db293b9879e0995
Zero Nelson <zero.nelson@abbott.com>
2023-07-13 14:48:32 -0500

    Add delimited list functionality

--- portscanner.py
+++ portscanner.py
@@ -4,44 +4,21 @@ import sys
 import re
 from datetime import datetime
 
-def scanMultiple(remServerIP):
-    startTime = datetime.now()
-    try:
-        for port in range(1,1065):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            result = sock.connect_ex((remServerIP, port))
-            sock.settimeout(0.1)
-            sock.setblocking(1)
-            if result == 0:
-                print(f"\nPort {port}:    Open")
-            else:
-                print(f"\nPort {port}:    Closed")
-            sock.close()
-
-    except KeyboardInterrupt:
-        print("Oops, you pressed CTRL+C")
-        subprocess.Popen('color 0F', shell=True)
-        sys.exit()
-
-    except socket.gaierror:
-        print("Oops, hostname could not be resolved.")
-        subprocess.Popen('color 0F', shell=True)
-        sys.exit()
-
-    except socket.error:
-        print("Couldn't connect to server")
-        subprocess.Popen('color 0F', shell=True)
-        sys.exit()
-
-    endTime = datetime.now()
-
-    time = endTime - startTime
-
-    print(f"\nElapsed Time: {time}")
-
 def scanOne(remServerIP, port):
     startTime = datetime.now()
     try:
+        if port == 0:
+            for port in range(1, 1065):
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                result = sock.connect_ex((remServerIP, port))
+                sock.settimeout(0.1)
+                sock.setblocking(1)
+                if result == 0:
+                    print(f"\nPort {port}:    Open")
+                else:
+                    print(f"\nPort {port}:    Closed")
+                sock.close()
+        else:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             result = sock.connect_ex((remServerIP, port))
             sock.settimeout(0.1)
@@ -73,18 +50,34 @@ def scanOne(remServerIP, port):
 
     print(f"\nElapsed Time: {time}")
 
-def getTarget():
-    print("""
-        ██████╗  ██████╗ ██████╗ ████████╗    ███████╗ ██████╗ █████╗ ███╗   ██╗███╗   ██╗███████╗██████╗ 
-        ██╔══██╗██╔═══██╗██╔══██╗╚══██╔══╝    ██╔════╝██╔════╝██╔══██╗████╗  ██║████╗  ██║██╔════╝██╔══██╗
-        ██████╔╝██║   ██║██████╔╝   ██║       ███████╗██║     ███████║██╔██╗ ██║██╔██╗ ██║█████╗  ██████╔╝
-        ██╔═══╝ ██║   ██║██╔══██╗   ██║       ╚════██║██║     ██╔══██║██║╚██╗██║██║╚██╗██║██╔══╝  ██╔══██╗
-        ██║     ╚██████╔╝██║  ██║   ██║       ███████║╚██████╗██║  ██║██║ ╚████║██║ ╚████║███████╗██║  ██║
-        ╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝       ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝
-                                                                                                        
-    """)
-    givenData = input("Enter full hostname or IP: ")
+def getPort():
+    port = int(input("Scan for particular port (Enter 0 to skip): "))
+    count = 0
+    while port > 65536:
+        if count == 0:
+            print(">>> Invalid port")
+        count += 1
+        if count == 2:
+            print(">>> One more chance")
+        if count == 3:
+            print("Exiting...")
+            sys.exit()
+        port = int(input("Scan for particular port (Enter 0 or port number): "))
+    return port
 
+def banner(port):
+    print("\n")
+    if int(port) > 0: 
+        print("*" * 52)
+        print(f"Scanning remote host ({remServerIP}) for port {port} ")
+        print("*" * 52)
+    else: 
+        print("*" * 52)
+        print(f"Scanning remote host ({remServerIP}) ")
+        print("*" * 52)
+
+def findTarget(target):
+    givenData = target
     ipPattern = '^(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$'
     ipMatch = bool(re.match(ipPattern, givenData))     
     hnPattern = '^[A-Za-z0-9]+\.[A-Za-z]+$'
@@ -118,44 +111,91 @@ def getTarget():
             remServerIP = socket.gethostbyname(givenData)
             return remServerIP
         
-def getPort():
-    port = int(input("Scan for particular port (Enter 0 to skip): "))
+def checkList(targetList, portList):
+    if not isValidTarget(targetList) and (int(portList) > 65536):
+        print("\n\033[4m Check your target(s) and port(s)...")
+        subprocess.Popen('color 0F', shell=True)
+        sys.exit()
+    if  isValidTarget(targetList) == False:
+        print(f"\nCheck your target(s)... {targetList} is not a valid target")
+        subprocess.Popen('color 0F', shell=True)
+        sys.exit()    
+    if int(portList) >  65536:
+        print(f"\nCheck your port(s)... {portList} is not a valid port")
+        subprocess.Popen('color 0F', shell=True)
+        sys.exit()
+
+def isValidTarget(target):
+    ipPattern = '^(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$'
+    ipMatch = bool(re.match(ipPattern, target))     
+    hnPattern = '^[A-Za-z0-9]+\.[Agit -Za-z]+$'
+    hnMatch = bool(re.match(hnPattern, target))
     count = 0
-    while port > 65536:
-        if count == 0:
-            print(">>> Invalid port")
-        count += 1
-        if count == 2:
-            print(">>> One more chance")
-        if count == 3:
-            print("Exiting...")
-            sys.exit()
-        port = int(input("Scan for particular port (Enter 0 or port number): "))
 
-    return port
-
-def banner(port):
-    print("\n")
-    if port: 
-        print("*" * 50)
-        print(f"Scanning remote host ({remServerIP}) for port {port} ")
-        print("*" * 50)
+    if ipMatch or hnMatch:
+       return True
     else: 
-        print("*" * 50)
-        print(f"Scanning remote host ({remServerIP}) ")
-        print("*" * 50)
-
-def scan(port):
-    if port != 0:
-        banner(port) 
-        scanOne(remServerIP, port)
-    else:
-        banner(port)
-        scanMultiple(remServerIP)
+        return False
 
 subprocess.call('cls', shell=True) # Clears screen
 subprocess.Popen('color 4', shell=True) # Change color
-remServerIP = getTarget()
-port = int(getPort())
-scan(port)
-subprocess.Popen('color 0F', shell=True)
+print("""
+        ██████╗  ██████╗ ██████╗ ████████╗    ███████╗ ██████╗ █████╗ ███╗   ██╗███╗   ██╗███████╗██████╗ 
+        ██╔══██╗██╔═══██╗██╔══██╗╚══██╔══╝    ██╔════╝██╔════╝██╔══██╗████╗  ██║████╗  ██║██╔════╝██╔══██╗
+        ██████╔╝██║   ██║██████╔╝   ██║       ███████╗██║     ███████║██╔██╗ ██║██╔██╗ ██║█████╗  ██████╔╝
+        ██╔═══╝ ██║   ██║██╔══██╗   ██║       ╚════██║██║     ██╔══██║██║╚██╗██║██║╚██╗██║██╔══╝  ██╔══██╗
+        ██║     ╚██████╔╝██║  ██║   ██║       ███████║╚██████╗██║  ██║██║ ╚████║██║ ╚████║███████╗██║  ██║
+        ╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝       ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝                                                                                                    
+""")
+
+target = input("Enter IPs/hostnames: ")
+targetPorts = input("Enter port(s) or 0 to skip: ")
+
+if  ("," in target and targetPorts) or ("," in target or targetPorts):
+    targetList = target.split(",")
+    portList = targetPorts.split(",")
+    targetList = [s.strip() for s in targetList]
+    portList = [s.strip() for s in portList]
+    if len(targetList) > len(portList):
+        for x in range(len(targetList)):
+            for y in range(len(portList)):
+                checkList(targetList[x], portList[y])
+                remServerIP = findTarget(targetList[x])
+                banner(portList[y])
+                scanOne(remServerIP, int(portList[y]))
+    elif len(targetList) < len(portList):
+        for x in range(len(portList)):
+            for y in range(len(targetList)):
+                checkList(targetList[y], portList[x])
+                remServerIP = findTarget(targetList[y])
+                banner(portList[x])
+                scanOne(remServerIP, int(portList[x]))
+    else:
+        for x in range(len(targetList)):
+            for y in range(len(portList)):
+                checkList(targetList[x], portList[y])
+                remServerIP = findTarget(targetList[x])
+                banner(portList[y])
+                scanOne(remServerIP, int(portList[y]))
+else:
+    checkList(target, targetPorts)
+    print("WE SHOULD BE HERE")
+    remServerIP = findTarget(target)
+    banner(targetPorts)
+    scanOne(remServerIP, int(targetPorts))
+
+print("""
+            █████████████████████████
+            ███████           ███████
+            ███████           ███████
+            ██                     ██
+            ██                     ██
+            ██                     ██
+            ██                     ██
+            ██    █   █   █   █    ██
+            ██    █   █   █   █    ██
+            ██    █   █   █   █    ██
+            █████████████████████████
+      """)
+subprocess.Popen('color 0F', shell=True) # Change color
+
